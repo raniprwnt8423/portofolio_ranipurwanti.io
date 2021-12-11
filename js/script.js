ec8e8c74e7076afd84d2c5fd12d5db2b29738982
raniprwnt8423 <94423845+raniprwnt8423@users.noreply.github.com>
2021-12-11 12:34:03 +0700

    upload
    upload portofolio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const txtElement = ['yang bercita-cita menjadi Wanita karir yang sukses '];
+let count =0;
+let txtIndex=0;
+let currentTxt='';
+let words='';
+
+(function ngetik(){
+	if(count==txtElement.length){
+		count=0;
+	}
+currentTxt=txtElement[count];
+
+words=currentTxt.slice(0,++txtIndex);
+document.querySelector('.efek-ngetik').textContent=words;
+
+if(words.length==currentTxt.length){
+	count++;
+	txtIndex=0;
+}
+setTimeout(ngetik, 200);
+})();
